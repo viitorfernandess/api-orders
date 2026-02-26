6cde72034a0b5b95ca9d5fbd72d31215c424c246
viitorfernandess <vitorazevedofer007@gmail.com>
2026-02-26 16:20:59 -0300

    refatoração: ajuste de formatação do código

--- src/model/orders-model.js
+++ src/model/orders-model.js
@@ -13,7 +13,6 @@ module.exports = {
         if (typeof productId !== 'number' || typeof quantity !== 'number' || total < 0) {
             return null
         }
-
         const newOrder = {
             id: uuid(),
             productId,
@@ -21,7 +20,6 @@ module.exports = {
             total,
             status: 'criado'
         }
-
         orders.push(newOrder)
         return newOrder
     },
@@ -31,24 +29,20 @@ module.exports = {
         if (orderIndex === -1) {
             return null
         }
-
         if (updatedOrder.quantity !== undefined) {
             if (typeof updatedOrder.quantity !== 'number' || updatedOrder.quantity <= 0) {
                 return null
             }
         }
-
         if (updatedOrder.total !== undefined) {
             if (typeof updatedOrder.total !== 'number' || updatedOrder.total <= 0) {
                 return null
             }
         }
-
         orders[orderIndex] = {
             ...orders[orderIndex],
             ...updatedOrder
         }
-
         return orders[orderIndex]
     },
 
