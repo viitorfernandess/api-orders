c2e26a74c74f5f347172907228b69b08f12d472c
viitorfernandess <vitorazevedofer007@gmail.com>
2026-03-12 15:21:19 -0300

    fx: corrige status code do erro de estoque inválido de 404 para 400

--- src/model/products-model.js
+++ src/model/products-model.js
@@ -50,7 +50,7 @@ module.exports = {
             throw new AppError("Preço inválido.", 400)
         }
         if (updatedProduct.stock !== undefined && (typeof updatedProduct.stock !== "number" || updatedProduct.stock < 0)) {
-            throw new AppError("Estoque insuficiente.", 404)
+            throw new AppError("Estoque insuficiente.", 400)
         }
         products[productIndex] = {
             ...products[productIndex],
