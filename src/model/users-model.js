d6a393a2e205fddbcae13666440a2188bfc0806b
viitorfernandess <vitorazevedofer007@gmail.com>
2026-03-18 14:45:44 -0300

    feat: adiciona método getUserById no users-model

--- src/model/users-model.js
+++ src/model/users-model.js
@@ -1,7 +1,16 @@
+
+const AppError = require('../errors/app-error')
+
 const users = []
 
 module.exports = {
     getAllUsers: () => users,
 
-    
+    getUserById: (id) => {
+        const user = users.find(user => user.id === id)
+        if (!user) {
+            throw new AppError('Usuário não encontrado', 404)
+        }
+        return user
+    }
 }
